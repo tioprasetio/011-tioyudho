fcc069c4efc664e10c7d80f340572e63a3102494
unknown <tioyudhoprasetio33@gmail.com>
2024-05-08 19:16:55 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\AccountController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+
+Route::group(['prefix' => 'account'], function(){
+    Route::group(['middleware' => 'guest'], function(){
+        Route::get('register', [AccountController::class, 'register'])->name('account.register');
+        Route::post('register', [AccountController::class, 'processRegister'])->name('account.processRegister');
+        Route::get('login', [AccountController::class, 'login'])->name('account.login');
+        Route::post('login', [AccountController::class, 'authenticate'])->name('account.authenticate');
+    });
+
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('profile', [AccountController::class, 'profile'])->name('account.profile');
+        Route::get('logout', [AccountController::class, 'logout'])->name('account.logout');
+        Route::post('update-profile', [AccountController::class, 'updateProfile'])->name('account.updateProfile');
+    });
+});
